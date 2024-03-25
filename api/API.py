7718580d967c5b8bcf2aa65fce2d8360773d3af2
cauamp <cauamp11@gmail.com>
2024-03-25 20:38:47 -0300

    melhora no tratamento de excessoes e correcao na comparacao de timestamp

--- api/API.py
+++ api/API.py
@@ -1,15 +1,15 @@
-
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from flask import Flask, jsonify
-from datetime import datetime, timedelta
-from .db_utils import *
 import psycopg2
+from .db_utils import *
+from datetime import datetime, timedelta
+from flask import Flask, jsonify
+from .config import *
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE=os.getenv("DATABASE_URL")
+DATABASE = os.getenv("DATABASE_URL")
+
 
 def connect_db():
     try:
@@ -20,87 +20,109 @@ def connect_db():
         print("Erro ao conectar ao banco de dados PostgreSQL:", e)
         return None
 
+
 def get_all_tables(cursor):
-	cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';")
-	tables = cursor.fetchall()
-	return [table[0] for table in tables]
+    cursor.execute(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';")
+    tables = cursor.fetchall()
+    return [table[0] for table in tables]
+
 
 @app.route('/api/<symbol>/getMetrics', methods=['GET'])
 def get_metrics_for_symbol(symbol):
-	symbol = symbol.upper()
-	conn = connect_db()
-	cursor = conn.cursor()
-	metrics = []
-	for table_name in get_all_tables(cursor):
+    symbol = symbol.upper()
+    conn = connect_db()
+    cursor = conn.cursor()
+    metrics = []
+    for table_name in get_all_tables(cursor):
+        try:
+            cursor.execute(
+                f'SELECT * FROM {table_name} WHERE Symbol = %s', (symbol,))
+            data = cursor.fetchall()
+            if data:
+                metrics.extend(data)
+                columns = [column[0] for column in cursor.description]
+                break
 
-		try:
-			cursor.execute(f'SELECT * FROM {table_name} WHERE Symbol = %s', (symbol,))
-			data = cursor.fetchall()
-			if data:
-				metrics.extend(data)
-				columns = [column[0] for column in cursor.description]
-				break
+        except psycopg2.Error as e:
+            print(f"Erro ao consultar tabela {table_name}: {e}")
+            return jsonify({"message": f"Erro ao consultar tabela {table_name}: {e}"})
 
-		except psycopg2.Error as e:
-			print(f"Erro ao consultar tabela {table_name}: {e}")
+    result_dict = {}
 
-	if metrics != [] and metrics is not None and 'timestamp' in metrics:
-		current_date = datetime.datetime.now()
-		data_timestamp = datetime.datetime.fromisoformat(metrics[0]['timestamp'])
-		time_difference = current_date - data_timestamp
-		if time_difference > datetime.timedelta(hours=24):
-			print("Informações disponiveis são antigas, atualizando... \n")
-			metrics = update_symbol(symbol, cursor)
-			if metrics is None:
-				return jsonify({"message": "Failed to update metrics."}), 500
+    if metrics == []:
+        print("Informações não encontradas para o símbolo especificado. Realizando pesquisa... \n")
+        try:
+            result_dict = update_symbol(symbol, cursor)
+        except BaseException as e:
+            return jsonify({"message": f"{e}"}), 500
+        except ... as e:
+            return jsonify({"message": f"Something went wrong when updating metrics. Exception: {e}"}), 500
 
-	result_dict = {}
+    else:
+        for i, column in enumerate(columns):
+            result_dict[column] = metrics[0][i]
 
-	if metrics == []:
-		print("Informações não encontradas para o símbolo especificado. Realizando pesquisa... \n")
-		result_dict = update_symbol(symbol, cursor)
-	else:
-		for i, column  in enumerate(columns):
-			result_dict[column] = metrics[0][i]
+        if metrics != {}:
+            current_date = datetime.datetime.now()
+            data_timestamp = datetime.datetime.fromisoformat(
+                str(result_dict['timestamp']))
+            time_difference = current_date - data_timestamp
+            if time_difference > datetime.timedelta(hours=24):
+                print("Informações disponiveis são antigas, atualizando... \n")
+                try:
+                    result_dict = update_symbol(symbol, cursor)
+                    if result_dict is None:
+                        return jsonify({"message": "Failed to update metrics."}), 500
+                except ... as e:
+                    return jsonify({"message": f"Something went wrong when updating metrics. Exception: {e}"}), 500
 
-	conn.commit()
-	conn.close()
+    conn.commit()
+    conn.close()
 
-	return jsonify(result_dict)
+    return jsonify(result_dict)
+
 
 @app.route('/api/<table_name>/getAssetsMetrics', methods=['GET'])
 def get_table(table_name):
-	table_name = table_name.upper()
-	conn = connect_db()
-	cursor = conn.cursor()
-	cursor.execute(f'SELECT * FROM {table_name}')
-	data = cursor.fetchall()
-	cursor.execute(f"PRAGMA table_info({table_name})")
-	columns = [column[1] for column in cursor.fetchall()]
+    table_name = table_name.upper()
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM {table_name}')
+    data = cursor.fetchall()
+    cursor.execute(f"PRAGMA table_info({table_name})")
+    columns = [column[1] for column in cursor.fetchall()]
 
-	conn.close()
+    conn.close()
 
-	result_list = []
-	result_dict = {}
+    result_list = []
+    result_dict = {}
 
-	for i in range(len(data)):
-		for j, column  in enumerate(columns):
-			result_dict[column] = data[i][j]
-		result_list.append(result_dict)
-		result_dict.clear
+    for i in range(len(data)):
+        for j, column in enumerate(columns):
+            result_dict[column] = data[i][j]
+        result_list.append(result_dict)
+        result_dict.clear
 
+    if result_list == []:
+        return jsonify({"message": "Failed to get metrics."}), 500
 
-	return jsonify(result_list)
+    return jsonify(result_list)
+
 
 @app.route('/api/<table_name>/getAssetsList', methods=['GET'])
 def get_table_list(table_name):
-	table_name = table_name.upper()
-	conn = connect_db()
-	cursor = conn.cursor()
-	cursor.execute(f'SELECT Symbol FROM {table_name}')
-	data = cursor.fetchall()
-	conn.close()		
-	return jsonify(data)
+    table_name = table_name.upper()
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT Symbol FROM {table_name}')
+    data = cursor.fetchall()
+    conn.close()
+    if data is None:
+        return jsonify({"message": "Failed to get assets list."}), 500
+
+    return jsonify(data)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)  # Desativar o modo de depuração
+    app.run(debug=True)  # Desativar o modo de depuração
--- api/db_utils.py
+++ api/db_utils.py
@@ -10,133 +10,138 @@ column_data_types = {
     "timestamp": "TIMESTAMP",
     "ask": "TEXT",
     "bid": "TEXT",
-	"day_s_range" : "TEXT"
+    "day_s_range": "TEXT"
 }
 
 
 def convert_to_float(value_str):
-	if 'N/A' in value_str.upper():	
-		return None
-	if ':' in value_str:
-		numerator, denominator = map(float, value_str.split(':'))
-		if denominator == 0:
-			return None  # Evita divisão por zero
-		return numerator / denominator
-	if re.search(r'^[\d.]+$', value_str[:-1]): 
-		if value_str.endswith('T'):
-			return float(value_str[:-1]) * 10**12
-		elif value_str.endswith('B'):
-			return float(value_str[:-1]) * 10**9
-		elif value_str.endswith('M'):
-			return float(value_str[:-1]) * 10**6
-		elif value_str.endswith('K'):
-			return float(value_str[:-1]) * 10**3
-		else:
-			return float(value_str)
-	else:
-		return value_str
+    if 'N/A' in value_str.upper():
+        return None
+    if ':' in value_str:
+        numerator, denominator = map(float, value_str.split(':'))
+        if denominator == 0:
+            return None  # Evita divisão por zero
+        return numerator / denominator
+    if re.search(r'^[\d.]+$', value_str[:-1]):
+        if value_str.endswith('T'):
+            return float(value_str[:-1]) * 10**12
+        elif value_str.endswith('B'):
+            return float(value_str[:-1]) * 10**9
+        elif value_str.endswith('M'):
+            return float(value_str[:-1]) * 10**6
+        elif value_str.endswith('K'):
+            return float(value_str[:-1]) * 10**3
+        else:
+            return float(value_str)
+    else:
+        return value_str
 
 
 def adjust_metrics_keys(metrics):
-	if metrics is not None:
-		new_data_dict = {}
-		for key, value in metrics.items():
+    if metrics is not None:
+        new_data_dict = {}
+        for key, value in metrics.items():
 
-			new_key = key.split('(')[0].strip()
-			if new_key != "Symbol":
-				new_key = new_key.lower()
+            new_key = key.split('(')[0].strip()
+            if new_key != "Symbol":
+                new_key = new_key.lower()
 
-			new_key = new_key.replace('%', 'percent')
-			new_key = re.sub(r'\W+', '_', new_key)
+            new_key = new_key.replace('%', 'percent')
+            new_key = re.sub(r'\W+', '_', new_key)
 
-			if not new_key[0].isdigit():
-				print("convertendo ", new_key, value, "\n")
+            if not new_key[0].isdigit():
 
-				if(new_key in ['Symbol', 'timestamp', 'type']):
-					new_data_dict[new_key] = value
-				else:
-					new_data_dict[new_key] = convert_to_float(value.replace('%', '').replace(',', ''))
+                if (new_key in ['Symbol', 'timestamp', 'type']):
+                    new_data_dict[new_key] = value
+                else:
+                    new_data_dict[new_key] = convert_to_float(
+                        value.replace('%', '').replace(',', ''))
 
-			if value == "index":
-				new_data_dict[new_key] = "currency"
+            if value == "index":
+                new_data_dict[new_key] = "currency"
 
-		metrics.clear()
-		metrics.update(new_data_dict)
+        metrics.clear()
+        metrics.update(new_data_dict)
 
 
 def create_table(cursor, table_name, columns):
 
-	cursor.execute(
-		"SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';")
-	tables = [table[0] for table in cursor.fetchall()]
+    cursor.execute(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';")
+    tables = [table[0] for table in cursor.fetchall()]
 
-	table_exists = table_name.lower() in tables
+    table_exists = table_name.lower() in tables
 
-	if not table_exists:
-		column_declarations = []
-		for column in columns:
-			for pattern, data_type in column_data_types.items():
-				if pattern in column.lower():
-					column_declarations.append(f"{column} {data_type}")
-					break 
-			else:
-				column_declarations.append(f"{column} FLOAT")		
-			
-		table_declaration = f"Symbol TEXT PRIMARY KEY, {', '.join(column_declarations)}"
-		print('\n\n\n\n\n', f"CREATE TABLE IF NOT EXISTS {table_name} ({table_declaration})", '\n\\n\n')
-		cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({table_declaration})")
+    if not table_exists:
+        column_declarations = []
+        for column in columns:
+            for pattern, data_type in column_data_types.items():
+                if pattern in column.lower():
+                    column_declarations.append(f"{column} {data_type}")
+                    break
+            else:
+                column_declarations.append(f"{column} FLOAT")
 
-	cursor.execute(
-		f"SELECT column_name FROM information_schema.columns WHERE table_name = %s", (table_name,))
-	existing_columns = [column[0] for column in cursor.fetchall()]
+        table_declaration = f"Symbol TEXT PRIMARY KEY, {', '.join(column_declarations)}"
+        print('\n\n\n\n\n',
+              f"CREATE TABLE IF NOT EXISTS {table_name} ({table_declaration})", '\n\\n\n')
+        cursor.execute(
+            f"CREATE TABLE IF NOT EXISTS {table_name} ({table_declaration})")
 
-	for column in columns:
-		if column not in existing_columns:
-			print("nao existia: ", column)
-			cursor.execute(
-				f"ALTER TABLE {table_name} ADD COLUMN {column} REAL")
+    cursor.execute(
+        f"SELECT column_name FROM information_schema.columns WHERE table_name = %s", (table_name,))
+    existing_columns = [column[0] for column in cursor.fetchall()]
+
+    for column in columns:
+        if column not in existing_columns:
+            print("nao existia: ", column)
+            cursor.execute(
+                f"ALTER TABLE {table_name} ADD COLUMN {column} REAL")
 
 
 def insert_data(cursor, table_name, data):
-	cursor.execute(
-		f"SELECT column_name FROM information_schema.columns WHERE table_name = %s", (table_name,))
-	table_columns = [column[0] for column in cursor.fetchall()]
+    cursor.execute(
+        f"SELECT column_name FROM information_schema.columns WHERE table_name = %s", (table_name,))
+    table_columns = [column[0] for column in cursor.fetchall()]
 
-	for key in data.keys():
-		if key not in table_columns and key != 'type' and key != 'Symbol':
-			raise ValueError(
-				f"A coluna '{key}' não existe na tabela '{table_name}'")
+    for key in data.keys():
+        if key not in table_columns and key != 'type' and key != 'Symbol':
+            raise ValueError(
+                f"A coluna '{key}' não existe na tabela '{table_name}'")
 
-	columns = ', '.join([col for col in data.keys() if col != 'type'])
+    columns = ', '.join([col for col in data.keys() if col != 'type'])
 
-	values = tuple(data[key] for key in data.keys() if key != 'type')
+    values = tuple(data[key] for key in data.keys() if key != 'type')
 
-	placeholders = ', '.join(['%s' for _ in range(len(values))])
+    placeholders = ', '.join(['%s' for _ in range(len(values))])
 
-	cursor.execute(
-		f"DELETE FROM {table_name} WHERE Symbol = %s", (data['Symbol'],))
+    cursor.execute(
+        f"DELETE FROM {table_name} WHERE Symbol = %s", (data['Symbol'],))
 
-	cursor.execute(
-		f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})", values)
+    cursor.execute(
+        f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})", values)
 
 
 def update_symbol(symbol, cursor):
-	try:
-		metrics = get_stock_metrics(symbol.upper())
+    try:
+        metrics = get_stock_metrics(symbol.upper())
 
-		if metrics is None:
-			return None
+        if metrics is None:
+            raise (f"Erro ao obter dados  do simbolo '{symbol}': {e}")
 
-		adjust_metrics_keys(metrics)
+        adjust_metrics_keys(metrics)
 
-		table_name = metrics['type']
-		create_table(cursor, table_name, [
-					 key for key in metrics.keys() if key != 'type' and key != 'Symbol'])
+        table_name = metrics['type']
+        create_table(cursor, table_name, [
+            key for key in metrics.keys() if key != 'type' and key != 'Symbol'])
 
-		insert_data(cursor, table_name, metrics)
+        insert_data(cursor, table_name, metrics)
 
-		return metrics
+        return metrics
 
-	except Exception as e:
-		print(f"Erro ao atualizar o símbolo '{symbol}': {e}")
-		return None
+    except BaseException as e:
+        raise BaseException(f"Erro ao atualizar o simbolo '{symbol}': {e}")
+
+    except ... as e:
+        print(f"Erro ao atualizar o símbolo '{symbol}': {e}")
+        raise Exception(f"Erro ao atualizar o simbolo '{symbol}': {e}")
